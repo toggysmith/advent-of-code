8336ed1db4a7a198f9a03d8f8991e20d989940a9
Toggy Smith <toggysmith@gmail.com>
2023-05-23 18:09:16 +0100

    feat(day-3, year-2015): add function to find number of houses with at least one present delivered

--- year-2015/day-3/part-1/main.cpp
+++ year-2015/day-3/part-1/main.cpp
@@ -5,7 +5,58 @@
 
 int
 get_number_of_houses_with_a_present(const std::string_view movements) {
-  return 0;
+  struct Location {
+    int x, y;
+  };
+
+  struct VisitedLocations {
+    std::vector<Location> visited_locations{
+      Location{0, 0},
+    };
+
+    void add(Location visited_location) {
+      const auto it{
+        std::find_if(visited_locations.begin(), visited_locations.end(), [visited_location](Location& location) {
+          return location.x == visited_location.x && location.y == visited_location.y;
+        })
+      };
+
+      if (it == visited_locations.end()) {
+        visited_locations.push_back(visited_location);
+      }
+    }
+
+    size_t size() { 
+      return visited_locations.size();
+    }
+  };
+
+  VisitedLocations visited_locations;
+
+  Location current_location{ 0, 0 };
+
+  for (const auto movement : movements) {
+    switch (movement) {
+      case '<':
+        current_location.x--;
+        visited_locations.add(current_location);
+        break;
+      case '>':
+        current_location.x++;
+        visited_locations.add(current_location);
+        break;
+      case '^':
+        current_location.y--;
+        visited_locations.add(current_location);
+        break;
+      case 'v':
+        current_location.y++;
+        visited_locations.add(current_location);
+        break;
+    }
+  }
+
+  return visited_locations.size();
 }
 
 bool
