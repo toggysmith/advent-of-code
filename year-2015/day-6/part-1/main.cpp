f0a5321c8b04bbdcb8c5e8ba2115360047fc2079
Toggy Smith <toggysmith@gmail.com>
2023-06-24 16:07:41 +0100

    complete part 1 of day 6 for 2015

--- year-2015/day-6/part-1/main.cpp
+++ year-2015/day-6/part-1/main.cpp
@@ -1,11 +1,82 @@
+#include <array>
 #include <fstream>
-#include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <vector>
 
 int get_no_of_lights_on(const std::vector<const std::string> actions) {
-  return 0;
+  std::array<std::array<bool, 1000>, 1000> lights;
+
+  for (int i = 0; i < lights.size(); i++) {
+    lights[i].fill(false);
+  }
+
+  const auto get_bounds_from_action =
+      [](const std::string &action) -> std::tuple<int, int, int, int> {
+    std::stringstream action_stream{action};
+    std::string action_part;
+    std::vector<std::string> action_parts;
+
+    while (std::getline(action_stream, action_part, ' ')) {
+      action_parts.push_back(action_part);
+    }
+    const auto convert_coords_to_values =
+        [](const std::string &coords) -> std::tuple<int, int> {
+      std::stringstream coords_stream{coords};
+      std::string coord_1, coord_2;
+
+      std::getline(coords_stream, coord_1, ',');
+      std::getline(coords_stream, coord_2, ',');
+
+      return std::make_tuple(std::stoi(coord_1), std::stoi(coord_2));
+    };
+
+    const auto [top_left_x, top_left_y] =
+        convert_coords_to_values(action_parts[0]);
+    const auto [bottom_right_x, bottom_right_y] =
+        convert_coords_to_values(action_parts[2]);
+
+    return std::make_tuple(top_left_x, top_left_y, bottom_right_x - top_left_x,
+                           bottom_right_y - top_left_y);
+  };
+
+  for (const auto &action : actions) {
+    if (action.substr(0, 7) == "turn on") {
+      const auto [x, y, w, h] =
+          get_bounds_from_action(action.substr(8, action.size()));
+      for (int i_x = x; i_x <= x + w; i_x++) {
+        for (int i_y = y; i_y <= y + h; i_y++) {
+          lights[i_x][i_y] = true;
+        }
+      }
+    } else if (action.substr(0, 8) == "turn off") {
+      const auto [x, y, w, h] =
+          get_bounds_from_action(action.substr(9, action.size()));
+      for (int i_x = x; i_x <= x + w; i_x++) {
+        for (int i_y = y; i_y <= y + h; i_y++) {
+          lights[i_x][i_y] = false;
+        }
+      }
+    } else {
+      const auto [x, y, w, h] =
+          get_bounds_from_action(action.substr(7, action.size()));
+      for (int i_x = x; i_x <= x + w; i_x++) {
+        for (int i_y = y; i_y <= y + h; i_y++) {
+          lights[i_x][i_y] = !lights[i_x][i_y];
+        }
+      }
+    }
+  }
+
+  int number_of_lights_on = 0;
+
+  for (int x = 0; x < 1'000; x++) {
+    for (int y = 0; y < 1'000; y++) {
+      number_of_lights_on += static_cast<int>(lights[x][y]);
+    }
+  }
+
+  return number_of_lights_on;
 }
 
 int main() {
@@ -18,7 +89,7 @@ int main() {
 
   std::vector<const std::string> actions;
 
-  while (input_file >> line) {
+  while (std::getline(input_file, line)) {
     actions.push_back(line);
   }
 
